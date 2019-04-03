075d5ac7c3a5a44baa294d3a9fcaa16010842ceb
Howard Bates <hbates@northmen.org>
2019-04-03 15:26:29 -0400

    Done.

--- app/src/main/java/com/codedelegance/thedicer/MainActivity.java
+++ app/src/main/java/com/codedelegance/thedicer/MainActivity.java
@@ -1,13 +1,125 @@
 package com.codedelegance.thedicer;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView rollsResult, totalResults, dieQTYValue;
+    private SeekBar dieQty;
+    private RadioGroup dieType;
+    private Button roll;
+    private int dieTypeValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rollsResult = findViewById(R.id.rollsResultsText);
+        totalResults = findViewById(R.id.totalResultsText);
+        dieQTYValue = findViewById(R.id.dieQTYValue);
+        dieQty = findViewById(R.id.dieQtySeekBar);
+        dieType = findViewById(R.id.dieTypeRadioGroup);
+        roll = findViewById(R.id.rollButton);
+
+        dieQTYValue.setText(dieQty.getProgress() + "/" + dieQty.getMax());
+        dieType.check(R.id.d4);
+
+        dieQty.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            final int MIN = 1;
+            int progress = 1;
+
+            @Override
+            public void onProgressChanged(SeekBar dieQty, int progressValue, boolean fromUser) {
+                progress = progressValue;
+                if (progress < MIN) {
+                    dieQty.setProgress(MIN);
+                }
+                dieQTYValue.setText(progress + "/" + dieQty.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar dieQty) {
+                // Do something here, if you want to do anything at the start of touching the seekbar
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar dieQty) {
+                dieQTYValue.setText(progress + "/" + dieQty.getMax());
+            }
+        });
+
+        dieType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup dieQty, int checkedId) {
+                if (checkedId == R.id.d2) {
+                    dieTypeValue = 2;
+                } else if (checkedId == R.id.d4) {
+                    dieTypeValue = 4;
+                } else if (checkedId == R.id.d6) {
+                    dieTypeValue = 6;
+                } else if (checkedId == R.id.d8) {
+                    dieTypeValue = 8;
+                } else if (checkedId == R.id.d10) {
+                    dieTypeValue = 10;
+                } else if (checkedId == R.id.d12) {
+                    dieTypeValue = 12;
+                } else if (checkedId == R.id.d20) {
+                    Toast.makeText(getApplicationContext(), "D20", Toast.LENGTH_LONG).show();
+                    dieTypeValue = 20;
+                } else {
+                    dieTypeValue = 100;
+                }
+            }
+        });
+
+        roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int dieQtyValue = dieQty.getProgress();
+
+                ProcessRoll dieRoll;
+                dieRoll = new ProcessRoll(dieQtyValue, dieTypeValue);
+
+                totalResults.setText(Integer.toString(dieRoll.getResult()));
+
+                int[] rolls = dieRoll.getRolls();
+                String rollText = "";
+                for (int i = 0; i < rolls.length; i++) {
+                    if (i == rolls.length - 1) {
+                        rollText = rollText + rolls[i];
+                    } else {
+                        rollText = rollText + rolls[i] + ", ";
+                    }
+                }
+                rollsResult.setText(rollText);
+            }
+        });
     }
+
+/*    public void handleRollButton(View view) {
+        int dieQtyValue = Integer.parseInt(dieQTYValue.getText().toString());
+        Toast.makeText(this, dieQtyValue, Toast.LENGTH_SHORT).show();
+
+        ProcessRoll dieRoll;
+        dieRoll = new ProcessRoll(dieQtyValue, dieTypeValue);
+
+        totalResults.setText(Integer.toString(dieRoll.getResult()));
+        Toast.makeText(this, Integer.toString(dieRoll.getResult()), Toast.LENGTH_SHORT).show();
+
+        int[] rolls = dieRoll.getRolls();
+        String rollText = "";
+        for (int i = 0; i < rolls.length; i++) {
+            if (i == rolls.length - 1) {
+                rollText = rollText + rolls[i];
+            } else {
+                rollText = rollText + rolls[i] + ", ";
+            }
+        }
+        rollsResult.setText(rollText);
+        Toast.makeText(this, rollText, Toast.LENGTH_SHORT).show();
+    }*/
 }
--- app/src/main/java/com/codedelegance/thedicer/ProcessRoll.java
+++ app/src/main/java/com/codedelegance/thedicer/ProcessRoll.java
@@ -2,14 +2,14 @@ package com.codedelegance.thedicer;
 
 import java.util.Random;
 
-    /**
-     * Created by bates.he.z on 5/7/2014.
-     */
-public class ProcessRoll {
+/**
+ * Created by bates.he.z on 4/3/2019.
+ */
+class ProcessRoll {
     private int result;
     private int[] rolls;
 
-    public ProcessRoll(int dieQty, int dieType, boolean total) {
+    ProcessRoll(int dieQty, int dieType) {
         result = 0;
         Random roll = new Random();
         rolls = new int[dieQty];
@@ -18,18 +18,16 @@ public class ProcessRoll {
         } else {
             for (int i = 0; i < dieQty; i++) {
                 rolls[i] = roll.nextInt(dieType) + 1;
-                if (total) {
-                    result = result + roll.nextInt(dieType) + 1;
-                }
+                result = result + roll.nextInt(dieType) + 1;
             }
         }
     }
 
-    public int getResult() {
+    int getResult() {
         return result;
     }
 
-    public int[] getRolls() {
+    int[] getRolls() {
         return rolls;
     }
 }
